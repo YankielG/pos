10095a0a947161805c6aacde81e37d33006ac6e4
AdamSkaznik <46684800+AdamSkaznik@users.noreply.github.com>
2020-04-01 18:49:27 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
