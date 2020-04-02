c2e803f9d993987a2b6c716d1b073ef7abef2525
AdamSkaznik <46684800+AdamSkaznik@users.noreply.github.com>
2020-04-02 15:39:34 +0200

    add Repository

--- src/main/java/pl/edu/wszib/pos/repository/EmployeeRepository.java
+++ src/main/java/pl/edu/wszib/pos/repository/EmployeeRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import pl.edu.wszib.pos.model.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    Employee findByeOIC (String eOIC);
+    Employee findByeEmail (String eEmail);
 }
