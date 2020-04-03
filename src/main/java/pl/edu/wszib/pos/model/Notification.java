d862622012e403f8fa7443fe075f49ece8a7a3d0
AdamSkaznik <46684800+AdamSkaznik@users.noreply.github.com>
2020-04-03 13:04:36 +0200

    add fragments

---
+++ src/main/java/pl/edu/wszib/pos/model/Notification.java
@@ -0,0 +1,15 @@
+package pl.edu.wszib.pos.model;
+
+import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.Entity;
+
+@Validated
+@Entity
+public class Notification {
+    private Long nId;
+
+
+
+
+}
