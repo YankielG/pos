133d6802f428d2f15e527b61e43070db080225bb
AdamSkaznik <46684800+AdamSkaznik@users.noreply.github.com>
2020-04-04 11:14:46 +0200

    add role and user to spring security

--- src/main/java/pl/edu/wszib/pos/model/User.java
+++ src/main/java/pl/edu/wszib/pos/model/User.java
@@ -1,15 +1,32 @@
 package pl.edu.wszib.pos.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer id;
-    String fName;
-    String lName;
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(name = "OIC")
+    private String OIC; //Osobisty Identyfiaktor Cyfrowy ("nick")
+    @Column(name = "password")
+    private String password;
+    @Column(name = "fName")
+    private String fName;
+    @Column(name = "lname")
+    private String lName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "active")
+    private boolean active;
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
     public Integer getId() {
         return id;
